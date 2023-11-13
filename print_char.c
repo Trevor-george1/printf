8e54dc2439f03ae5aeefe918dd748d6970a87586
Trevor <Trevormahinda@gmai.com>
2023-11-13 13:30:48 +0300

    Task 0

--- print_char.c
+++ print_char.c
@@ -7,7 +7,16 @@
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+	char *char_buff = (char *)malloc(2);
 
-	write(1, &c, 1);
+	if (char_buff == NULL)
+	{
+		return (0);
+	}
+	char_buff[0] = c;
+	char_buff[1] = '\0';
+
+	write(1, char_buff, 1);
+	free(char_buff);
 	return (1);
 }
