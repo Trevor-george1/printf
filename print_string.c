1a8bb8cb5c23bc2cf8ea11e9102abe07f981e4e8
Trevor <Trevormahinda@gmai.com>
2023-11-13 16:42:28 +0300

    Update the string file

--- print_string.c
+++ print_string.c
@@ -49,7 +49,7 @@ int print_string_S(va_list args)
 			s_buff[3] = (s[printed] % 16) + (s[printed] % 16 > 9 ? 'A' - 10 : '0');
 			s_buff[4] = '\0';
 
-			write(1, s_buff, _strlen(s_buff));
+			write(1, s_buff, strlen(s_buff));
 			printed += 1;
 			free(s_buff);
 		}
