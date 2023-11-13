17dd2e7b913aa2f20c2173ad36766d2b083e2796
Trevor <Trevormahinda@gmai.com>
2023-11-14 02:06:58 +0300

    update printf

--- _printf.c
+++ _printf.c
@@ -13,6 +13,14 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+	{
+		return (-1);
+	}
+	if (format[0] == '%' && format[1] == '' && !format[2])
+	{
+		return (-1);
+	}
 	/** otherwise loop throught the format string*/
 	while (*format)
 	{
