30aa5c71d68ee61bef6a5ac76204573e8a9178e0
Trevor <Trevormahinda@gmai.com>
2023-11-13 14:15:04 +0300

    update hex

--- print_hex.c
+++ print_hex.c
@@ -8,17 +8,46 @@
 int print_hex(va_list args, int uppercase)
 {
 	unsigned int value = va_arg(args, unsigned int);
-	char hex_buff[12];
-	char *hex_str;
+	char hex_digit[8];
+	int count = 0, digit;
 
-	snprintf(hex_buff, sizeof(hex_buff), uppercase ? "%X" : "%x", value);
-	hex_str = strdup(hex_buff);
-	if (hex_str == NULL)
+	if (value == 0)
 	{
-		return (0);
+		write(1, "0", 1);
+		count++;
 	}
-	write(1, hex_str, strlen(hex_str));
-	free(hex_str);
-	return (strlen(hex_str));
+	while (value > 0)
+	{
+		digit = value % 16;
+		hex_digit[count] = digit < 10 ? '0' + digit :
+		(uppercase ? 'A' : 'a') + digit - 10;
+		value /= 16;
+		count++;
+	}
+	while (count > 0)
+	{
+		count--;
+		write(1, &hex_digit[count], 1);
+	}
+	return (count);
+}
+
+/**
+ * print_unsigned_hex - print unsiged hex
+ * @arg: arguments
+ * Return: count
+ */
+int print_unsigned_hex(va_list arg)
+{
+	return (print_hex(arg, 0));
+}
+/**
+ * print_unsigned_hex_upper - prints hex in upper
+ * @arg: arguments
+ * Return: count
+ */
+int print_unsigned_hex_upper(va_list arg)
+{
+	return (print_hex(arg, 1));
 }
 
